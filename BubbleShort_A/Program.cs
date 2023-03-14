dc7848b4dbfbfb4fe3bd58acc5aba38fc9f7ebf2
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2023-03-14 11:04:51 +0700

    Add Documentation

--- BubbleShort_A/Program.cs
+++ BubbleShort_A/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 namespace Buble_Sort
 {
+    /// <summary>
+    /// Class <c>Program</c> berisi variabel dan method untuk membaca data
+    /// </summary>
     class Program
     {
         //Deklarasi array int dengan ukuran 20
@@ -9,21 +12,27 @@ namespace Buble_Sort
         //Deklarasi variabel int untuk menyimpan banyaknya data pada array
         private int n;
 
-        //Fungsi / Method untuk menerima masukan
+        /// <summary>
+        /// Method <c>read</c>Fungsi / Method untuk menerima masukan
+        /// </summary>
         public void read()
         {
             //menerima angka untuk menentukan banyaknya data yang disimpan pada array
             while (true)
             {
+                //menampilkan tulisan yang sebagai wadah untuk input data string
                 Console.Write("Masukkan banyaknya elemen pada Array: ");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
+                //jika n lebih dari 20 maka akan mendapat notifikasi maksimal 20 elemen
                 if (n <= 20)
                     break;
                 else
                     Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
 
             }
+
+            //menampilkan tulisan input elemen
             Console.WriteLine("");
             Console.WriteLine("-----------------------");
             Console.WriteLine(" Masukkan elemen array ");
@@ -36,10 +45,12 @@ namespace Buble_Sort
                 string s1 = Console.ReadLine();
                 a[i] = Int32.Parse(s1);
             }
-            
 
-            
         }
+
+        /// <summary>
+        ///Method <c>display</c>untuk menampilkan data yang terdapat pada program
+        /// </summary>
         public void display()
         {
             //Menampilkan array yang tersusun
@@ -47,12 +58,17 @@ namespace Buble_Sort
             Console.WriteLine("----------------------------------");
             Console.WriteLine(" Elemen Array yang telah tersusun ");
             Console.WriteLine("----------------------------------");
+            //untuk nilai j = 0, j kurang dari n dan j++, maka akan menampilkan array yang tersusun
             for (int j = 0; j < n; j++)
             {
                 Console.WriteLine(a[j]);
             }
             Console.WriteLine("");
         }
+
+        /// <summary>
+        /// Method <c>BubbleSortArray</c>Method untuk program bublesort yang mengurutkan antar elemen menggunakan array
+        /// </summary>
         public void BubbleSortArray()
         {
             for (int i = 1; i < n; i++) //For n - 1 passes
@@ -73,6 +89,10 @@ namespace Buble_Sort
             }
         }
 
+        /// <summary>
+        /// Method <c>Main</c>Method Main atau program utama untuk menjalankan program
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             //Membuat Objek class dari BubbleSort
